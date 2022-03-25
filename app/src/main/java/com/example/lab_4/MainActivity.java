ac496458f562743ba517b53398ebf2fcfcd1e779
Narzaru <yaroslahek@gmail.com>
2022-03-25 12:04:31 +0700

    add button

--- app/src/main/java/com/example/lab_4/MainActivity.java
+++ app/src/main/java/com/example/lab_4/MainActivity.java
@@ -2,20 +2,43 @@ package com.example.lab_4;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.FrameMetricsAggregator;
+
 import android.graphics.drawable.AnimationDrawable;
+import android.location.GnssAntennaInfo;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button startStopButton;
+    private ImageView img;
+    private AnimationDrawable frameAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView img = findViewById(R.id.imageView);
+        startStopButton = findViewById(R.id.button);
+        img = findViewById(R.id.imageView);
         img.setBackgroundResource(R.drawable.rabbit_animation);
-        AnimationDrawable frameAnimation = (AnimationDrawable)img.getBackground();
+        frameAnimation = (AnimationDrawable)img.getBackground();
         frameAnimation.setOneShot(false);
+
+        startStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (frameAnimation.isRunning()) {
+                    frameAnimation.stop();
+                } else {
+                    frameAnimation.start();
+                }
+            }
+        });
+
         frameAnimation.start();
     }
 }
